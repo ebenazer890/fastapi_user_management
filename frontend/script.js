78d9a5ba5cf056e12cd88d481a74a99e057fb443
ebenazer890 <ebenazer890@gmail.com>
2026-08-18 16:57:34 +0530

    Add API package initializer

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://fastapi-user-management-4cgx.onrender.com';
 
 const userForm = document.getElementById('userForm');
 const updateUserForm = document.getElementById('updateUserForm');
